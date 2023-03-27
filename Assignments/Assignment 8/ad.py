1603ab29f73345a68104461772e755e61da4055d
MatteoPassalent <127155708+MatteoPassalent@users.noreply.github.com>
2023-03-27 16:04:13 -0400

    Add files via upload

---
+++ Assignments/Assignment 8/ad.py
@@ -0,0 +1,39 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Your Name
+ID:      Your ID
+Email:   your email@mylaurier.ca
+__updated__ = "2022-11-30"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+
+def func():
+    """
+    -------------------------------------------------------
+    description
+    Use: 
+    -------------------------------------------------------
+    Parameters:
+        name - description (type)
+    Returns:
+        name - description (type)
+    ------------------------------------------------------
+    """
+
+
+def calculate(number):
+    return number * 4
+
+
+def arithmetic(number):
+    return number - 4
+
+
+print(arithmetic(3))
+print(calculate(3))
